a50f49d86694b171cdb632e72f2de662c75df790
bruceey42 <48457426+bruceey42@users.noreply.github.com>
2021-02-10 09:35:50 +0100

    Docstring and plotting updates
    Added patches to plots with a legend showing layer and boundary condition labels. Will consider making the legend location and other arguements keyword argumenets in future updates. Moved some usage examples to a script.

--- RadialExact1D.py
+++ RadialExact1D.py
@@ -48,13 +48,43 @@ heat transfer at any radial position can be trivially calculated.
 import numpy as np
 import pandas as pd
 from matplotlib import pyplot as plt
+from matplotlib import cm
+from matplotlib.patches import Rectangle
 class layer:
+    """
+    Parent class for the other layer types, takes care of some methods like 
+    assigning common variables.
+    
+    Parameters
+    ----------
+        t: float
+            Required; the layer thickness [m]
+        label: str
+            Optional; a descriptive string used for labeling the layer.
+    """
     def __init__(self,  t, label):
         if t<=0:
             raise ValueError('please enter positive non-zero layer thickness for {}'.format(self))
         self.t = t #layer thickness [m]
         self.label=label
 class solid_layer(layer):
+    """
+    Class to manage input assignment for solid layers such as metals and
+    ceramics.
+    
+    Parameters
+    ----------
+        t: float
+            Required; the layer thickness [m]
+        k: float
+            Required; thermal conductivity [W/mK]
+        V_heat: float
+            Optional; volumetric heat generation [W/m^3], default 0.
+        R_out: float
+            Optional; thermal resistance of the outer interface [m^2K/W]
+        label: str
+            Optional; a descriptive string used for labeling the layer.
+    """
     def __init__(self, t, k, V_heat = 0, R_out = 0, label=None):
         super().__init__(t,label)
         self.layer_type = 'solid'
@@ -62,15 +92,68 @@ class solid_layer(layer):
         self.V_heat = V_heat #volumetric heating [W/m^3] 
         self.R_out = R_out #thermal resistance at outer edge [m^2K/W]
 class fluid_layer(layer):
+    """
+    Class to manage variable assignment of fluid layers. The assumption for
+    fluid layers is that the coolant flow rate is sufficient to prevent
+    significant axial thermal gradients.
+    
+    Parameters
+    ----------
+        t: float
+            Required; the layer thickness [m]
+        T_inf: float
+            Required; the bulk coolant temperature [K]
+        h_in: float
+            Required; heat transfer coefficient on inner annulus [W/m^2K]
+        h_out: float
+            Required; heat transfer coefficient on outer annulus [W/m^2K]
+        label: str
+            Optional; a descriptive string used for labeling the layer.
+    """
     def __init__(self, t, T_inf, h_in, h_out, label=None):
         super().__init__(t, label)
         self.layer_type = 'fluid'
-        self.T_inf = T_inf #bulk coolant temperature [degC]
+        self.T_inf = T_inf #bulk coolant temperature [K]
         if (h_in <= 0 or h_out <= 0):
             raise ValueError('Please enter positive, non-zero HTC values for {}'.format(self))
         self.h_in = h_in #HTC at inner radial surface [W/m^2K]
         self.h_out = h_out #HTC at outer radial surface [W/m^2K]
 class boundary_condition:
+    """
+    Class for boundary conditions, takes care of some methods like assigning
+    common variables.
+    
+    Parameters
+    ----------
+    Required keyword arguments depend on the boundary condition type supplied.
+        BC_type: str ('temperature', 'fluid' or 'heat flux'')
+            Required; defines the type of boundary condition.
+        BC_side: str ('inner' or 'outer')
+            Required; applies the boundary condition to the inner or outer
+            edge of the assembly.
+        **kwargs:
+            For BC_type 'temperature':
+                T_inf; float
+                    Required: the temperature of the boundary [K].
+                R; float
+                    Optional: the contact resistance [m^2K/W], default 0.
+                label; str
+                    Optional: a descriptive string used for labeling the layer.
+            For BC_type 'fluid':
+                T_inf; float
+                    Required: the bulk coolant temperature [K].
+                h; float
+                    Required: Heat transfer coefficient [W/m^2K] cannot be 0.
+                label; str
+                    Optional: a descriptive string used for labeling the layer.
+            For BC_type 'heat flux':
+                q; float
+                    Required: heat flux applied to the boundary in radial dir,
+                    [W/m^2]
+                label; str
+                    Optional: a descriptive string used for labeling the layer.
+                
+    """
     def __init__(self, BC_type, BC_side, **kwargs):
         kword_dict = {'temperature':{'T_inf':1,'R':0,'label':0},
                       'fluid':{'T_inf':1,'h':1,'label':0},
@@ -162,19 +245,25 @@ class RadialExact1D:
     def C(self):
         
         """
-        Returns integration constants to fully define temperature profile,
+        Assigns integration constants to fully define temperature profile,
         two are needed to define the temperature profile in each solid layer.
         The system of equations are set up using 2 boundary conditions,
         A heat continuity equation for each solid-solid boundary.
         A temperature equation for each boundary.
+        The form of the equation solved is AC=B, where C (the array of
+        integration constants are to be solved for). The least squares method
+        is used as with certain layer combinations result in singular A
+        matrices.
         -------
         """
         def A_for_T_bound(sgn,r,k,R):
+            #integration constant coefficients
             return np.array([[sgn*k*R/r-np.log(r), -1]])
         def B_for_T_bound(sgn,r,k,R,V,T_inf):
+            #RHS of ODE solution
             return V*r/2*(sgn*R-r/(2*k)) - T_inf
         #initialise matrices
-        N=self.solid_map.count(1)
+        N=self.solid_map.count(1) #count number of solid layers
         A_, B_ = np.zeros((2*N,2*N)), np.zeros((2*N,1))
         j=0 # row tracker
         #apply boundary conditions:
@@ -197,14 +286,15 @@ class RadialExact1D:
                 A_[j,2*n:2*n+2]=A_for_T_bound(sgn,r,k,R)
                 B_[j,0] = B_for_T_bound(sgn,r,k,R,V,BC_.T_inf)
                 j+=1
-            else:
+            else: #for heat flux BC
                 A_[j,2*n:2*n+2]=np.array([[1, 0]])
                 B_[j,0] = -r/k*(BC_.q - V*r/2)
                 j+=1    
         #apply layer temp and heat continuity
         for i in range(len(self.layers)-1):
-            i_sol = sum(self.solid_map[0:i])
+            i_sol = sum(self.solid_map[0:i]) #solid layer index
             if self.layers[i].layer_type == self.layers[i+1].layer_type:
+                #statement only triggered for solid-solid interfaces as fluid-fluid layer arrangements are not allowed.
                 #heat continuity
                 r=self.radii[i+1]
                 k0=self.layers[i].k
@@ -220,6 +310,7 @@ class RadialExact1D:
                 B_[j,0]=r/2*(V0*(R+r/(2*k0))-V1*r/(2*k1))
                 j+=1
             elif self.layers[i].layer_type == 'solid':
+                #only triggered when going from solid-fluid
                 #define based on solid-fluid transition
                 r=self.radii[i+1] 
                 k=self.layers[i].k
@@ -240,13 +331,12 @@ class RadialExact1D:
                 j+=1
             else:
                 pass
-        self.A_=A_
-        self.B_=B_
-        C_ = np.linalg.lstsq(A_,B_,rcond=None)[0]
-        self.C_=C_
+        self.A_=A_ #store LHS
+        self.B_=B_ #store RHS
+        C_ = np.linalg.lstsq(A_,B_,rcond=None)[0] #solve integration constants
+        self.C_=C_ #store result as attribute
     def layer_index_for_r(self,rad):
         """
-
         Parameters
         ----------
         rad : int or float
@@ -267,6 +357,19 @@ class RadialExact1D:
                 if rad >= self.radii[i] and rad< self.radii[i+1]:
                     return i            
     def T(self,rad):
+        """
+        Returns temperature [K] given any radial position [m]
+        
+        Parameters
+        ----------
+        rad : float
+            radial position from center [m].
+
+        Returns
+        -------
+        T: float
+            Temperature at position [K].
+        """
         layer_i = self.layer_index_for_r(rad)
         if layer_i == 'inner':
             if not self.BC_in:
@@ -295,6 +398,19 @@ class RadialExact1D:
             elif layer.layer_type=='solid':
                 return self.T_solid(rad,layer,layer_i)[0]
     def q(self,rad):
+        """
+        Returns heat flux [W/m^2] given any radial position [m].
+        
+        Parameters
+        ----------
+        rad : float
+            radial position from center [m].
+
+        Returns
+        -------
+        q: float, np.nan
+            heat flux [W/m^2]. Returns np.nan if not in defined system range.
+        """
         #calculating heat flux at a radial position
         if not hasattr(self,'C_'):
             self.C()
@@ -312,32 +428,74 @@ class RadialExact1D:
         
         return
     def Q(self,rad):
+        """
+        Returns heat transfer for unit length [W/m] given any radial position
+        [m]
+        
+        Parameters
+        ----------
+        rad : float
+            radial position from center [m].
+
+        Returns
+        -------
+        q: float
+            heat transfer [W/m]. Returns np.nan if not in defined system range.
+        """
         #calculating heat transfer at a radial position
         return self.q(rad)*2*np.pi*rad
         
         return
     def T_solid(self,rad,layer,layer_i):
+        """
+        Parameters
+        ----------
+        rad : float
+            radial position from center [m].
+        layer : solid_layer object
+            The solid layer in which to calculate the temperature.
+        layer_i : int
+            layer index of the layer in which to calculate the temperature.
+
+        Returns
+        -------
+        float
+            Temperature [K]
+        """
+    
         if not hasattr(self,'C_'):
             self.C()
         V_heat, k = layer.V_heat, layer.k
         solid_layer_i = sum(self.solid_map[0:layer_i])
         CA, CB = self.C_[2*solid_layer_i],self.C_[2*solid_layer_i+1]
         return -V_heat/(4*k)*rad**2+CA*np.log(rad)+CB
-    def plot_T(self, dpoints=100, plotQ=1, plotq=1, show_bounds=1, Q_labels=1):
+    def plot_T(self, dpoints=100,
+               plotQ=1,
+               plotq=1,
+               show_bounds=1,
+               Q_labels=0,
+               patches=0, 
+               ):
         """
         Plots the temperature profile through the wall thickness.
         Parameters
         ----------
         dpoints : int
             number of datapoints to plot through whole wall thickness.
+            The default is 100.
         plotQ : boolean (0,1, True, False) optional
             If True plots heat transfer through solid layers, else does not.
-            The default is 0.
+            The default is 1.
         plotq : boolean (0,1, True, False) optional
             If True plots heat flux through solid layers, else does not.
-            The default is 0.
+            The default is 1.
         show_bounds : boolean (0,1, True, False) optional
             If true labels layers. Draw layer boundaries.
+            The default is 0.
+        Q_labels : boolean (0,1, True, False) optional
+            If true labels layers by drawing patches in layer and adding a
+            legend.
+            The default is 0.
         Returns
         -------
         figure
@@ -379,7 +537,7 @@ class RadialExact1D:
                 else:
                     ax1.plot([self.radii[i+1]*1e3,self.radii[i+1]*1e3],[min(T),max(T)],'k-',linewidth=0.5)
         ax1.set_xlabel('Radius [mm]')
-        ax1.set_ylabel(r'Temperature $^{\circ}$C', color='r')
+        ax1.set_ylabel(r'Temperature K', color='r')
         ax1.plot(rads*1e3,T,'r-', label='Temperature')
         if plotq == 1 and plotQ==1:
             #offset axis to stop overlap
@@ -446,9 +604,85 @@ class RadialExact1D:
                          verticalalignment='center',
                          horizontalalignment='center',
                          )
-                i+=1 
+                i+=1
+        #Make some patches with labels and a legend.
+        if patches==1:
+            #find unique layers
+            unique_layers=set(self.layers)
+            unique_labels={} #init dicts for storing unique labels
+            i={} #count number of identical labels for different objects
+            i['unnamed layer'],i['unnamed BC']=1,1 #add counter for unlabelled layers
+            #Assign labels to each unique layer.
+            for layer in unique_layers:
+                try:
+                    if layer.label==None:
+                        unique_labels[layer]='unnamed layer {}'.format(i['unnamed layer'])
+                        i['unnamed layer']+=1
+                    else:
+                        unique_labels[layer]=(layer.label+' {}'.format(i[layer.label]))
+                        i[layer.label]+=1
+                except KeyError:
+                    unique_labels[layer]=layer.label
+                    i[layer.label]=2
+            #Assign labels to each unique BC.
+            for BC in self.BC:
+                if BC.label==None:
+                    unique_labels[BC]='unnamed BC {}'.format(i['unnamed BC'])
+                    i['unnamed BC']+=1
+                else:
+                    try:
+                        unique_labels[BC]=(BC.label+' BC {}'.format(i[BC.label+' BC']))
+                        i[BC.label+' BC']+=1
+                    except KeyError:
+                        unique_labels[BC]=BC.label + ' BC'
+                        i[BC.label+' BC']=2
+            N=len(unique_labels)
+            patch_list=[]
+            i=0
+            col = {} #colours corresponding to labels
+            prop = np.linspace(0,1,N) #equispaced sampling for colour spetrum
+            for (key,val) in unique_labels.items():
+               col[key]=cm.rainbow(prop[i],alpha=0.2) 
+               i+=1
+            i=0
+            ymin = ax1.get_ylim()[0]
+            yrange = ax1.get_ylim()[1]-ymin
+            for layer in self.layers:
+                colour = col[layer]
+                ax1.add_patch(Rectangle((self.radii[i]*1e3,ymin),layer.t*1e3,
+                              yrange,
+                              color=colour,
+                              label=unique_labels[layer]))
+                i += 1
+            if len(self.BC_in)==1:
+                colour=col[self.BC_in[0]]
+                xmin = ax1.get_xlim()[0]
+                xmax = ax1.get_xlim()[1]
+                xrange_inner = self.radii[0]*1e3-xmin
+                xrange_outer = xmax-self.radii[-1]*1e3
+                ax1.add_patch(Rectangle((xmin,ymin),xrange_inner,
+                              yrange,
+                              color=colour,
+                              label=unique_labels[self.BC_out[0]]))
+                colour=col[self.BC_out[0]]
+                ax1.add_patch(Rectangle((self.radii[-1]*1e3,ymin),xrange_outer,
+                              yrange,
+                              color=colour,
+                              label=unique_labels[self.BC_out[0]]))
+                patch_list.append(Rectangle((0,0),0,0,
+                                            color=col[self.BC_in[0]],
+                                            label=unique_labels[self.BC_in[0]]))
+                patch_list.append(Rectangle((0,0),0,0,
+                                            color=col[self.BC_out[0]],
+                                            label=unique_labels[self.BC_out[0]]))
+            for layer in unique_layers:
+                patch_list.append(Rectangle((0,0),0,0,
+                                            color=col[layer],
+                                            label=unique_labels[layer]))
+        ax1.legend(handles=patch_list, loc='best')
         fig.tight_layout()
         return fig
+
     def results(self):
         """
         Returns a dataframe object with summarising all the inputs and main
@@ -468,20 +702,20 @@ class RadialExact1D:
             if layer.layer_type=='solid':
                 Ti = self.T(self.radii[i])
                 To = self.T(self.radii[i+1]*(1-1e-10))
-                row['T_{r_inner} [degC]'] = Ti
-                row['T_{r_outer} [degC]'] = To
-                row['T_min [degC]'] = min([Ti,To])
+                row['T_{r_inner} [K]'] = Ti
+                row['T_{r_outer} [K]'] = To
+                row['T_min [K]'] = min([Ti,To])
                 #check if there is the possibility of a max temp higher than
                 #boundary temperatures (i.e. if you set dT/dr=0 do you get
                 #a real radius value... for this CA must be positive.
                 if self.C_[2*i_sol]>0:
                     r_Tmax = (2*layer.k*self.C_[2*i_sol]/layer.V_heat)**0.5
                     if r_Tmax<=self.radii[i+1] and r_Tmax>=self.radii[i]:
-                        row['T_max [degC]']=self.T(r_Tmax)
+                        row['T_max [K]']=self.T(r_Tmax)
                     else:
-                        row['T_max [degC]']=max([Ti,To])
+                        row['T_max [K]']=max([Ti,To])
                 else:
-                    row['T_max [degC]']=max([Ti,To])
+                    row['T_max [K]']=max([Ti,To])
                 Qi = self.Q(self.radii[i])
                 Qo = self.Q(self.radii[i+1]*(1-1e-10))
                 Qvol = layer.V_heat*np.pi*(self.radii[i+1]**2-self.radii[i]**2)
@@ -493,10 +727,10 @@ class RadialExact1D:
             elif layer.layer_type=='fluid':
                 Ti = self.T(self.radii[i]*(1-1e-10))
                 To = self.T(self.radii[i+1])
-                row['T_{r_inner} [degC]']=Ti
-                row['T_{r_outer} [degC]']=To
-                row['T_max [degC]']=max([Ti,To,layer.T_inf])
-                row['T_min [degC]']=min([Ti,To,layer.T_inf])
+                row['T_{r_inner} [K]']=Ti
+                row['T_{r_outer} [K]']=To
+                row['T_max [K]']=max([Ti,To,layer.T_inf])
+                row['T_min [K]']=min([Ti,To,layer.T_inf])
                 Qi = self.Q(self.radii[i]*(1-1e-10))
                 Qo = self.Q(self.radii[i+1])
                 Qnet = Qi-Qo
@@ -530,25 +764,10 @@ class RadialExact1D:
             'BC_type':'boundary condition type',
             'h_in':'inner heat transfer coefficient [W/m^2K]',
             'h_out':'outer heat transfer coefficient [W/m^2K]',
-            'T_inf':'Bulk temperature [degC]',
+            'T_inf':'Bulk temperature [K]',
             'q':'boundary heat flux [W/m^2]',
             'R':'boundary thermal resistance [m^2K/W]'
             }
-        df=pd.DataFrame(rows)
-        df=df.rename(columns=column_head)
+        df=pd.DataFrame(rows) #make the dataframe
+        df=df.rename(columns=column_head) #rename the column headings
         return df
-a = solid_layer(0.5e-3,30,1e7,1e-5,'Eurofer wall')
-b = fluid_layer(1e-3,480,1e4,1e4, 'He interlayer')
-c = solid_layer(0.2e-3,10,2e7,1e-5, 'Surface coating')
-BC1 = boundary_condition('temperature', 'outer',T_inf=450, R=1e-6)
-BC2 = boundary_condition('fluid', 'inner', T_inf=600, h=1e4)
-BC3 = boundary_condition('fluid', 'outer', T_inf=400, h=1e4, label='outer fluid')
-BC4 = boundary_condition('fluid', 'inner', T_inf=400, h=1e4, label='inner fluid')
-BCHFout = boundary_condition('heat flux', 'outer',q=1e6)
-BCHFin = boundary_condition('heat flux', 'inner',q=-1e6)
-A = RadialExact1D(5e-3,[c,a,c,b,c,a,c],BC3,BC2)
-B = RadialExact1D(5e-3,[c,a,c],BC3,BCHFout)
-C=RadialExact1D(5e-3,[c,a,c],BC3,BC4)
-c4_1=solid_layer(0.5e-3,300,0,1e-5)
-c4_2=solid_layer(3.5e-3,30,1e8,0)
-Comp_c4 = RadialExact1D(5e-3,[c4_1,c4_2,b,c4_1,c4_2],BC2,BC3)
